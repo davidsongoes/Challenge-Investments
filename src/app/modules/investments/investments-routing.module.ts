feb150b28d8de637b95b662df20bf062d1dd9d72
Davidson Góes <davidsongoes@gmail.com>
2022-03-20 19:25:39 -0300

    feat(route): :truck: Implementa rotas iniciais

---
+++ src/app/modules/investments/investments-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { InvestmentsListComponent } from "./components/investments-list/investments-list.component";
+
+const routes: Routes = [
+  {
+    path: "",
+    component: InvestmentsListComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class InvestmentsRoutingModule {}
---
+++ src/app/modules/investments/investments.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import { InvestmentsRoutingModule } from "./investments-routing.module";
+import { InvestmentsListComponent } from "./components/investments-list/investments-list.component";
+import { SharedModule } from "src/app/shared/shared.module";
+import { FlexLayoutModule } from "@angular/flex-layout";
+
+@NgModule({
+  declarations: [InvestmentsListComponent],
+  imports: [
+    CommonModule,
+    InvestmentsRoutingModule,
+    SharedModule,
+    FlexLayoutModule,
+  ],
+})
+export class InvestmentsModule {}
