337356dc0ea1a4eff6471925e78144a60a617dd3
Davidson Góes <davidsongoes@gmail.com>
2022-03-22 23:12:20 -0300

    refactor(list): :recycle: Refatora lista para ajustes

--- src/app/modules/investments/components/investments-list/investments-list.component.ts
+++ src/app/modules/investments/components/investments-list/investments-list.component.ts
@@ -1,21 +1,44 @@
 import { Observable } from "rxjs";
-import { Component, OnInit } from "@angular/core";
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
 import { InvestmentsFacade } from "../../investments-facade";
 import { Investments } from "./../../../../models/investments.model";
+import { Router } from "@angular/router";
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from "@angular/animations";
 
 @Component({
   selector: "inv-investments-list",
   templateUrl: "./investments-list.component.html",
   styleUrls: ["./investments-list.component.scss"],
+  animations: [
+    trigger("fadeIn", [
+      state("*", style({ opacity: 1 })),
+      transition(":enter", [
+        style({ opacity: 0 }),
+        animate("600ms 0s ease-in-out"),
+      ]),
+    ]),
+  ],
 })
 export class InvestmentsListComponent implements OnInit {
-  title: string = "Investment - List";
-  data: Observable<Investments[]> = new Observable<Investments[]>();
+  title: string = "Investimentos - Lista";
+  data$: Observable<Investments[]> = new Observable<Investments[]>();
   displayedColumns: string[] = ["nome", "objetivo", "saldoTotal"];
 
-  constructor(private service: InvestmentsFacade) {}
+  constructor(private service: InvestmentsFacade, private router: Router) {}
 
   ngOnInit(): void {
-    this.data = this.service.findAll();
+    this.data$ = this.service.findAll();
+  }
+
+  actionDetail(data: Investments) {
+    this.router.navigateByUrl("investments/detail", {
+      state: data,
+    });
   }
 }
