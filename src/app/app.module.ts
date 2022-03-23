cb3b2a1807be916522643a80d2e51f73483e2b13
Davidson Góes <davidsongoes@gmail.com>
2022-03-22 23:08:44 -0300

    feat(locale): :globe_with_meridians: Implementa locale e currency BRL

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { LayoutModule } from "./layout/layout.module";
-import { NgModule } from "@angular/core";
+import { LOCALE_ID, NgModule, DEFAULT_CURRENCY_CODE } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-
+import localePt from "@angular/common/locales/pt";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { HttpClientModule } from "@angular/common/http";
+import { registerLocaleData } from "@angular/common";
+
+registerLocaleData(localePt, "pt");
 
 @NgModule({
   declarations: [AppComponent],
@@ -18,7 +21,10 @@ import { HttpClientModule } from "@angular/common/http";
     FlexLayoutModule,
     HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: "pt-BR" },
+    { provide: DEFAULT_CURRENCY_CODE, useValue: "BRL" },
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
