d21e8bea368e963f7c80403e16ca948f53132192
Davidson Góes <davidsongoes@gmail.com>
2022-03-22 23:06:03 -0300

    feat(material): :sparkles: Implementa novos componentes

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -4,6 +4,9 @@ import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
 import { MatCardModule } from "@angular/material/card";
 import { MatTableModule } from "@angular/material/table";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
   declarations: [],
@@ -13,6 +16,9 @@ import { MatTableModule } from "@angular/material/table";
     MatButtonModule,
     MatCardModule,
     MatTableModule,
+    MatProgressSpinnerModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
 })
 export class MaterialModule {}
