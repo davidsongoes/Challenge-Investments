5737de8f13ee81f14dcb99cc5292866a732bbb62
Davidson Góes <davidsongoes@gmail.com>
2022-03-23 20:05:51 -0300

    feat(pipe): :sparkles: Implementa pipe sanitize

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,14 @@
+import { FlexLayoutModule } from "@angular/flex-layout";
 import { MaterialModule } from "./material/material.module";
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { GenericDialogComponent } from "./generic-dialog/generic-dialog.component";
+import { OverlayModule } from "@angular/cdk/overlay";
+import { SanitizeHtmlPipe } from "./pipes/sanitize-html.pipe";
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
-  exports: [MaterialModule],
+  declarations: [GenericDialogComponent, SanitizeHtmlPipe],
+  imports: [CommonModule, MaterialModule, OverlayModule, FlexLayoutModule],
+  exports: [MaterialModule, GenericDialogComponent, SanitizeHtmlPipe],
 })
 export class SharedModule {}
