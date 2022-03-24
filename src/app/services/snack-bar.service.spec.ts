25afc2db93d83b758f92a98e93a09a179d6336ca
Davidson Góes <davidsongoes@gmail.com>
2022-03-23 22:26:19 -0300

    test(snack-bar): ✅ Implementa teste padrão para o snack bar service

---
+++ src/app/services/snack-bar.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed } from "@angular/core/testing";
+import { SnackbarService } from "./snack-bar.service";
+
+describe("SnackBarService", () => {
+  let service: SnackbarService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(SnackbarService);
+  });
+
+  it("should be created", () => {
+    expect(service).toBeTruthy();
+  });
+});
